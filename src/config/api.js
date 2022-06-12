5f187c282392c4e8cd5bf3c8c7012a9dc8f7b05e
Yandi Riswandi <yandiriswandi@gmail.com>
2022-06-13 03:15:53 +0700

    version 2.0

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,8 @@
 export const API = () => {
-  const baseUrl = "http://localhost:5000/api/v1";
+  const baseUrl =
+  process.env.REACT_APP_SERVER_URL ||
+  'https://b32-backend-yandi.herokuapp.com/api/v1' ||
+  'https://localhost:5000/api/v1';
 
   const executeAPI = async (endpoint, config) => {
     const response = await fetch(baseUrl + endpoint, config);
