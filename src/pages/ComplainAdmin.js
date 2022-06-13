d6ede994d6182b67455d2d37322cd0beaef285c3
Yandi Riswandi <yandiriswandi@gmail.com>
2022-06-13 12:01:16 +0700

    version 1.1

--- src/pages/ComplainAdmin.js
+++ src/pages/ComplainAdmin.js
@@ -29,7 +29,7 @@ export default function ComplainAdmin() {
     const [state] = useContext(UserContext)
 
     useEffect(() =>{
-        socket = io('https://dumbmerch-percobaan.herokuapp.com' || 'http://localhost:5000', {//ketika dimounting akan mengisikan socket dan memunculkan
+        socket = io('https://dumbmerch-percobaan.herokuapp.com/api/v1' || 'http://localhost:5000', {//ketika dimounting akan mengisikan socket dan memunculkan
             auth: {                           //client connect
                 token: localStorage.getItem('token')//kirim ke backend line 17
             },
